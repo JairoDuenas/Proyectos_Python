5ce5180bfed461a6d9a62d474dd2884be15668ac
JairoDuenas <jairoduenas.ing@gmail.com>
2023-03-27 14:37:57 -0500

    Quiz program

---
+++ quiz_program/quiz_program.py
@@ -0,0 +1,58 @@
+# A dictionary that stores questions and answers
+# Have a variable that the score of the player
+# Loop through the dictionary using the key values pairs
+# Display each questions to the user and allow then to answer
+# Tell them if they are right or wrong
+# Show the final result when quiz is completed
+
+quiz = {
+  "question1": {
+    "question": "What is the capital of France?",
+    "answer": "Paris"
+  },
+  "question2": {
+    "question": "What is the capital of Germany?",
+    "answer": "Berlin"
+  },
+  "question3": {
+    "question": "What is the capital of Italy?",
+    "answer": "Rome"
+  },
+  "question4": {
+    "question": "What is the capital of Spain?",
+    "answer": "Madrid"
+  },
+  "question5": {
+    "question": "What is the capital of Portugal?",
+    "answer": "Lisbon"
+  },
+  "question6": {
+    "question": "What is the capital of Switzerland?",
+    "answer": "Bern"
+  },
+  "question7": {
+    "question": "What is the capital of Austria?",
+    "answer": "Vienna"
+  },
+}
+
+score = 0
+for key, value in quiz.items():
+  print(value['question'])
+  answer = input("Answer? ")
+
+  if answer.lower() == value['answer'].lower():
+    print('Correct')
+    score = score + 1
+    print(f"Your score is: " + str(score))
+    print("")
+    print("")
+  else:
+    print("Wrong!")
+    print("The answer is: " + value["answer"])
+    print("Ypour score is: " + str(score))
+    print("")
+    print("")
+
+print("Your got " + str(score) + " out of 7 questions correctly")
+print("Your percentage is " + str(int(score / 7 * 100)) + "%")
