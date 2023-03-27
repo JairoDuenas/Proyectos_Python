cd6df0e59a8123440d9e92d0cc5ed4c1525388fb
JairoDuenas <jairoduenas.ing@gmail.com>
2023-03-27 10:25:24 -0500

    Reemplazar palabra

---
+++ reemplazar_palabra/reemplazar_palabra.py
@@ -0,0 +1,7 @@
+def replace_word():
+  str = "Hi guys, i am Jairo, and hi hi hi hi hi"
+  word_to_replace = input("Enter thr word to repalce: ")
+  word_replacement = input("Enter the word replacement:")
+  print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
